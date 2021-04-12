888ae97a50379fc3094074d937818eec038fef7c
Kitanin Sergei <kitanin.ser@mail.ru>
2021-04-12 18:06:26 +0300

    rename file

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 """This file sets up a command line manager.
 
-Use "python main.py" for a list of available commands.
-Use "python main.py runserver" to start the development web server on localhost:5000.
-Use "python main.py runserver --help" for a list of runserver options.
+Use "python manage.py" for a list of available commands.
+Use "python manage.py runserver" to start the development web server on localhost:5000.
+Use "python manage.py runserver --help" for a list of runserver options.
 """
 import os
 
@@ -18,8 +18,8 @@ manager.add_command('db', MigrateCommand)
 manager.add_command('init_db', InitDbCommand)
 
 if __name__ == "__main__":
-    # python main.py                      # shows available commands
-    # python main.py runserver --help     # shows available runserver options
+    # python manage.py                      # shows available commands
+    # python manage.py runserver --help     # shows available runserver options
     # manager.run({'init_db': InitDbCommand()})
     # port = int(os.environ.get("PORT", 5000))
     # manager.app.run(host='0.0.0.0', port=port)
