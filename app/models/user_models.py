c1b95e3799b6da74a02233e653e8e2e7249cbbff
Kitanin Sergei <kitanin.ser@mail.ru>
2021-04-12 22:26:23 +0300

    update

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.Unicode(50), nullable=False, server_default=u'')
     last_name = db.Column(db.Unicode(50), nullable=False, server_default=u'')
 
-    api_key = db.Column(db.Unicode(100), nullable=False, server_default=u'')
+    api_key = db.Column(db.Unicode(40), nullable=False, server_default=u'')
 
     # Relationships
     roles = db.relationship('Role', secondary='users_roles',
