3a2f8eb26b55961679a6d7bc5853d7d1cfb77066
Kitanin Sergei <kitanin.ser@mail.ru>
2021-04-12 21:05:44 +0300

    update

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,12 @@
+import os
+
 from app import create_app
+from app.commands.init_db import init_db
 
 app = create_app()
+
+if __name__ == '__main__':
+    with app.app_context():
+        init_db()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
